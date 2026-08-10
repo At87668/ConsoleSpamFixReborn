211337ad721ea152f1e2b7722c0ba737049a6628
At87668 <author87668@hotmail.com>
2026-08-10 17:10:51 +0800

    refactor(velocity): remove duplicated EngineInterface, now shared in core

--- src/main/java/link/star_dust/consolefix/velocity/EngineInterface.java
+++
@@ -1,9 +0,0 @@
-package link.star_dust.consolefix.velocity;
-
-public interface EngineInterface {
-    public void hideConsoleMessages();
-
-    public int getHiddenMessagesCount();
-
-    public void addHiddenMsg();
-}
