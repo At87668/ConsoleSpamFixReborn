6d8103c1cbf736e0c0811e696cc55781d97e890e
At87668 <author87668@hotmail.com>
2026-08-10 17:01:55 +0800

    refactor(bukkit): move CommandHandler to bukkit package

---
+++ src/main/java/link/star_dust/consolefix/bukkit/CommandHandler.java
@@ -0,0 +1,39 @@
+package link.star_dust.consolefix.bukkit;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ * Bukkit command executor for {@code /csf reload}.
+ */
+public class CommandHandler implements CommandExecutor {
+    private final CSF csf;
+
+    public CommandHandler(CSF csf) {
+        this.csf = csf;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String cmdlabel, String[] args) {
+        if (cmdlabel.equalsIgnoreCase("csf")) {
+            if (args.length == 1) {
+                if (args[0].equalsIgnoreCase("reload")) {
+                    if (sender instanceof Player) {
+                        if (!sender.hasPermission("csf.admin")) {
+                            sender.sendMessage(this.csf.getConfigHandler().getStringWithColor("ChatMessages.NoPermission"));
+                            return true;
+                        }
+                        sender.sendMessage(this.csf.getConfigHandler().getStringWithColor("ChatMessages.CmdReload"));
+                    }
+                    this.csf.getConfigHandler().loadConfig();
+                    this.csf.getLogFilterManager().updateFilter();
+                }
+            } else {
+                sender.sendMessage(this.csf.getConfigHandler().getStringWithColor("ChatMessages.CmdHelp"));
+            }
+        }
+        return true;
+    }
+}
