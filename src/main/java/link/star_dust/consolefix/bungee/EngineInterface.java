cbcd73c78e63e6bdff94faa9e6a1ad4f8aa74254
At87668 <author87668@hotmail.com>
2026-08-10 17:08:03 +0800

    refactor(bungee): remove duplicated EngineInterface, now shared in core

--- src/main/java/link/star_dust/consolefix/bungee/EngineInterface.java
+++
@@ -1,9 +0,0 @@
-package link.star_dust.consolefix.bungee;
-
-public interface EngineInterface {
-    public void hideConsoleMessages();
-
-    public int getHiddenMessagesCount();
-
-    public void addHiddenMsg();
-}
